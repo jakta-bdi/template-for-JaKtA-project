e331250c4f8f992ff24239b2a37cc272dab6a5aa
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-11 13:27:29 +0100

    ci(renovate):  use `with` to import the configuration instead of `assert` (#877)

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@ const publishCmd = `
 ./gradlew uploadKotlin release || exit 1
 ./gradlew publishKotlinOSSRHPublicationToGithubRepository || true
 `
-import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
     [
         "@semantic-release/exec",
